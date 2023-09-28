91b01a933eb4bcb776684ebde323fd2b59656a44
Double-Light <103574126+Double-Light@users.noreply.github.com>
2023-09-28 17:26:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// 取得搜尋按鈕相關元素
+const searchIcon = document.getElementById('search-icon');
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const searchBtnClose = document.getElementById('search-btn-close');
+
+searchBtn.addEventListener('click', function() {
+   // 檢查是否包含 "active" 類
+   if (searchBtn.classList.contains('active')) {
+     // 執行頁面跳轉
+     window.location.href = "./allcourses.html?keyword=" + searchInput.value;
+   } else {
+     // 新增 "active" 類別
+     searchIcon.classList.add('active');
+   }
+});
+
+// 給 #searchBtnClose 按鈕添加點擊事件
+searchBtnClose.addEventListener('click', function() {
+  searchIcon.classList.remove('active');
+});
